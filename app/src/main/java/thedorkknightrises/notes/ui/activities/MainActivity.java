ab266e217a30724cbde712bfd40ab95e57045db1
TheDorkKnightRises <samriddhabasu@gmail.com>
2019-03-15 05:56:34 +0530

    Version 3.3.3 - Fix status bar icon visibility in light theme activities

--- app/src/main/java/thedorkknightrises/notes/ui/activities/MainActivity.java
+++ app/src/main/java/thedorkknightrises/notes/ui/activities/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends AppCompatActivity
 
         lightTheme = pref.getBoolean(Constants.LIGHT_THEME, false);
         if (lightTheme)
-            setTheme(R.style.AppTheme_Light_NoActionBar);
+            setTheme(R.style.AppTheme_Light_DrawerActivity);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
